640e018a7e03f9214a65b45e3d352405e1989a32
Jordan Harris <jordan.lee.harris@icloud.com>
2022-09-22 15:09:33 +0100

    updates

---
+++ Pages/13_Nerdy_stuff.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+from Team_Energy_app import Show_Graph, forecast, test_df, figure, test_wd
+
+with st.container():
+    st.empty()
+# st.json(r.json)
