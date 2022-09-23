61975337598c654fb3b099f3782c8788f6815dac
Jordan Harris <jordan.lee.harris@icloud.com>
2022-09-23 17:22:08 +0100

    test

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"jordan.lee.harrs@icloud.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+streamlit run [entrypoint file]
